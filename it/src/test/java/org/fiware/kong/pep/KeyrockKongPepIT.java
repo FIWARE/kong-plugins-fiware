f45a35d18da6a40127159b2a5d389bd0f2241e7d
Stefan Wiedemann <wistefan@googlemail.com>
2022-07-08 14:55:10 +0200

    fix resource

--- it/src/test/java/org/fiware/kong/pep/KeyrockKongPepIT.java
+++ it/src/test/java/org/fiware/kong/pep/KeyrockKongPepIT.java
@@ -105,7 +105,7 @@ public class KeyrockKongPepIT {
 		keyrockPermission.action = "GET";
 		keyrockPermission.name = "Test";
 		keyrockPermission.isRegex = true;
-		keyrockPermission.resource = "/orion-keyrock/*";
+		keyrockPermission.resource = "/*";
 		permissionContainer.permission = keyrockPermission;
 
 		HttpResponse<String> permissionCreateResponse = HttpClient.newHttpClient().send(
