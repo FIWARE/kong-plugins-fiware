9eef6bc1be0eab02f289b3ccd423ce1f983e579b
Stefan Wiedemann <stefan.wiedemann@fiware.org>
2022-12-12 14:25:30 +0100

    Update keyrock.go

--- kong-pep-plugin/keyrock.go
+++ kong-pep-plugin/keyrock.go
@@ -8,10 +8,7 @@ import (
 
 	cache "github.com/patrickmn/go-cache"
 	log "github.com/sirupsen/logrus"
-)
-
-// http client to be used for accessing keyrock
-var authorizationHttpClient httpClient = &http.Client{}
+}
 
 // implementation of the PDP-inteface for keyrock
 type KeyrockPDP struct{}
